9bda0a2a47599550438848a9b8f334e36098e816
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-17 12:46:20 +0900

    hypervisor.c: hadded __init and __exit

--- hypervisor.c
+++ hypervisor.c
@@ -6,13 +6,13 @@ MODULE_AUTHOR("Ucchiee");
 MODULE_DESCRIPTION("uchv: ucchiee's hypervisor");
 MODULE_VERSION("0.0.1");
 
-static int my_init(void)
+int __init my_init(void)
 {
   printk(KERN_INFO "Hello world.\n");
   return  0;
 }
 
-static void my_exit(void)
+static void __exit my_exit(void)
 {
   printk(KERN_INFO "Goodbye world.\n");
 
